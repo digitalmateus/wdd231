077129dea9dd99076c8f16992e0f6cdc72df938b
digitalmateus <mnunes@byupathway.edu>
2025-05-14 22:35:20 -0300

    changes on my js

---
+++ scripts/prophets.js
@@ -0,0 +1,34 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {  //Create a async defined function named "getProphetData" t
+    const response = await fetch(url); // fetch data from the JSON source url using the await fetch() method.
+    const data = await response.json(); //Store the response and convert from the fetch() method in a const variable
+    //console.table(data.prophets);
+    displayProphets(data.prophets); 
+}
+
+getProphetData(); // Call the function to fetch the data
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+    
+    let card = document.createElement('section');
+    let fullName = document.createElement('H2');
+    let portrait = document.createElement('img');
+
+    
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // Build the h2 content out to show the prophet's full name
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${fullName}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  }); // end of arrow function and forEach loop    
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,85 +1,25 @@
 const currentYear = new Date().getFullYear();
 const lastModified = document.lastModified; 
 
-const courses = [
-    {
-        subject: 'CSE',
-        number: 110,
-        title: 'Introduction to Programming',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
-        technology: [
-            'Python'
-        ],
-        completed: true
-    },
-    {
-        subject: 'WDD',
-        number: 130,
-        title: 'Web Fundamentals',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
-        technology: [
-            'HTML',
-            'CSS'
-        ],
-        completed: true
-    },
-    {
-        subject: 'CSE',
-        number: 111,
-        title: 'Programming with Functions',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
-        technology: [
-            'Python'
-        ],
-        completed: true
-    },
-    {
-        subject: 'CSE',
-        number: 210,
-        title: 'Programming with Classes',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
-        technology: [
-            'C#'
-        ],
-        completed: false
-    },
-    {
-        subject: 'WDD',
-        number: 131,
-        title: 'Dynamic Web Fundamentals',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
-        technology: [
-            'HTML',
-            'CSS',
-            'JavaScript'
-        ],
-        completed: true
-    },
-    {
-        subject: 'WDD',
-        number: 231,
-        title: 'Frontend Web Development I',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
-        technology: [
-            'HTML',
-            'CSS',
-            'JavaScript'
-        ],
-        completed: false
-    }
-]
+const btnAll = document.getElementById('btn-all');
+const btnCse = document.getElementById('cse');
+const btnWdd = document.getElementById('wdd');
+
+const cse110 = document.getElementById('cse110');
+const wdd130 = document.getElementById('wdd130');
+const cse111 = document.getElementById('cse111');
+const cse210 = document.getElementById('cse210');
+const wdd131 = document.getElementById('wdd131');
+const wdd231 = document.getElementById('wdd231');
+
+btnAll.addEventListener('click', function() {
+    cse110.style.backgroundColor = 'black';
+    cse111.style.backgroundColor = 'black';
+    cse210.style.backgroundColor = 'black';
+    wdd130.style.backgroundColor = 'black';
+    wdd131.style.backgroundColor = 'black';
+    wdd231.style.backgroundColor = 'black';
+});
 
 
 document.addEventListener("DOMContentLoaded", function () {
